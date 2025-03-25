aac98a886dc7debc5dff29bf3b3adb4d2ff45efb
profesor <profesor@dawdistancia.net>
2025-03-25 21:15:44 +0100

    fix

---
+++ config/soap-client.php
@@ -0,0 +1,54 @@
+<?php
+
+use Phpro\SoapClient\CodeGenerator\Assembler;
+use Phpro\SoapClient\CodeGenerator\Rules;
+use Phpro\SoapClient\CodeGenerator\Config\Config;
+use Soap\ExtSoapEngine\ExtSoapOptions;
+use Phpro\SoapClient\Soap\CodeGeneratorEngineFactory;
+
+return Config::create()
+    ->setEngine($engine = CodeGeneratorEngineFactory::create(
+        'https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl'
+    ))
+    ->setTypeDestination('src\Type')
+    ->setTypeNamespace('Clases\Type')
+    ->setClientDestination('src')
+    ->setClientName('Pub_gestdocenteClient')
+    ->setClientNamespace('Clases')
+    ->setClassMapDestination('src')
+    ->setClassMapName('Pub_gestdocenteClassmap')
+    ->setClassMapNamespace('Clases')
+    ->addRule(new Rules\AssembleRule(new Assembler\GetterAssembler(new Assembler\GetterAssemblerOptions())))
+    ->addRule(new Rules\AssembleRule(new Assembler\ImmutableSetterAssembler(
+        new Assembler\ImmutableSetterAssemblerOptions()
+    )))
+    ->addRule(
+        new Rules\IsRequestRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\RequestAssembler()),
+                new Rules\AssembleRule(new Assembler\ConstructorAssembler(new Assembler\ConstructorAssemblerOptions())),
+            ])
+        )
+    )
+    ->addRule(
+        new Rules\IsResultRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\ResultAssembler()),
+            ])
+        )
+    )
+    ->addRule(
+        new Rules\IsExtendingTypeRule(
+            $engine->getMetadata(),
+            new Rules\AssembleRule(new Assembler\ExtendingTypeAssembler())
+        )
+    )
+    ->addRule(
+        new Rules\IsAbstractTypeRule(
+            $engine->getMetadata(),
+            new Rules\AssembleRule(new Assembler\AbstractClassAssembler())
+        )
+    )
+;
